95eb84db401b2d5c0c3aae6782249cc8da43e154
loyd010 <94154952+loyd010@users.noreply.github.com>
2022-01-27 21:49:26 -0600

    Added array containing pokemon character information

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
-alert('Hello world');
-
-let favoriteFood = 'Vegetable lasagna';
-document.write(favoriteFood);
+let pokemonList = [
+  {name: 'Jigglypuff', height: 2, type: ['normal','fairy']},
+  {name: 'Squirtle', height: 2, type: 'water'},
+  {name: 'Dugtrio', height: 2, type: 'ground'},
+  {name: 'Butterfree', height: 4, type: ['bug','flying']},
+  {name: 'Ivysaur', height: 3, type: ['grass','poison']}
+];
