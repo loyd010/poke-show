8a350b0556fdfc8142fda8154e2ea2308d4f665a
loyd010 <94154952+loyd010@users.noreply.github.com>
2022-01-31 20:58:42 -0600

    Added comments to describe code intent

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//This code creates an array with pokemon names and characteristics
 let pokemonList = [
   {name: 'Jigglypuff', height: 2, type: ['normal','fairy']},
   {name: 'Squirtle', height: 2, type: 'water'},
@@ -6,7 +7,7 @@ let pokemonList = [
   {name: 'Ivysaur', height: 3, type: ['grass','poison']}
 ];
 
-
+//This code creates a loop to print the name and height of each pokemon character
 for (let i=0; i<pokemonList.length; i++){
   document.write(pokemonList[i].name + ' (' + 'height: ' + pokemonList[i].height + '\'' + ')' + '<br>');
 }
