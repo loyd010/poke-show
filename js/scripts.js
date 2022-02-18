881105e38db8b5167850716f338de522747d8569
loyd010 <94154952+loyd010@users.noreply.github.com>
2022-02-18 15:49:25 -0600

    Added modal to buttons to display pokemon details and added styling and layout changes to the buttons

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ let pokemonRepository = (function(){
   let pokemonList = [];
 
 //Added link to API for pokemon data
-  let apiURL = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
   function add(pokemon) {
     pokemonList.push(pokemon);
@@ -31,14 +31,14 @@ let pokemonRepository = (function(){
 //updated showDetails function to show details retrieved via API
   function showDetails(pokemon){
     loadDetails(pokemon).then(function (){
-    console.log(pokemon);
+    showModal(pokemon);
     });
   }
 
 //added loadList and loadDetails functions to pokemon repository
 
   function loadList(){
-    return fetch(apiURL).then(function (response){
+    return fetch(apiUrl).then(function (response){
       return response.json();
     }).then(function (json){
       json.results.forEach(function (item){
@@ -66,13 +66,63 @@ let pokemonRepository = (function(){
     });
   }
 
+//added functions to show and hide modal
+let modalContainer = document.querySelector('.modal-container');
+
+  function showModal(pokemon){
+    modalContainer.innerHTML = '';
+
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click', hideModal);
+
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = (pokemon.name);
+
+    let contentElement = document.createElement('p');
+    contentElement.innerText = ('Height: ' + pokemon.height + '\'');
+
+    let imageElement = document.createElement('img');
+    imageElement.src = (pokemon.imageUrl);
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modal.appendChild(imageElement);
+    modalContainer.appendChild(modal);
+
+    modalContainer.classList.add('is-visible');
+  }
+
+  function hideModal(){
+    modalContainer.classList.remove('is-visible');
+  }
+
+  //added event listeners to close modal if clicking outside of modal or pressing escape
+  window.addEventListener('keydown', (e) => {
+    if(e.key === 'Escape' && modalContainer.classList.contains('is-visible')){
+      hideModal();
+    }
+  });
+
+  modalContainer.addEventListener('click', (e) => {
+    let target = e.target;
+    if(target === modalContainer){
+      hideModal();
+    }
+  });
+
   return {
     add: add,
     getAll: getAll,
     addListItem: addListItem,
     loadList: loadList,
     loadDetails: loadDetails,
-    showDetails: showDetails
+    showDetails: showDetails,
   };
 })();
 
